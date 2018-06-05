fc38236ab13adbd3996e4855e5f6a85c40b2078f
Pol Layola Izquierdo <pol_layola@MacBook-Pro-de-Pol.local>
2018-06-05 19:19:58 +0200

    transfer coap-client into a different repository

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# node-coap-client
+# Node CoAP Client
+
+## Internet of Things CoAP Client using NodeJs
+
+### Start here:
+```
+git clone https://github.com/playola/node-coap-client.git
+
+cd node-coap-client
+
+npm install coap --save
+
+node node-coap-client.js
+```
+
+### References
+* https://github.com/mcollina/node-coap
---
+++ node-coap-client.js
@@ -0,0 +1,57 @@
+/*
+* Author: Pol Layola
+* Date: June 2018
+*
+* Node CoAP Client
+* CoAP Client that observe a sensor and sends its value to a platform.
+* The platforms thethings.io needs a TOKEN_ID to post data into their database.
+*
+* Default port 5638. Default action 'GET'
+*
+* References: https://github.com/mcollina/node-coap
+*/
+var coap = require('coap');
+var server = coap.createServer({ type: 'udp6' });
+
+server.on('request', function(req, res) {
+  res.end('Starting CoAP Client \n')
+})
+
+server.listen(function() {
+  var req = coap.request({
+    host: 'COAP_IP',
+    pathname: '/sensors/pressure',
+    observe: true,
+    method: 'GET'
+  });
+
+  req.on('response', function(res) {
+    var response = res.pipe(process.stdout);
+    updateDatabase(response);
+  })
+
+  req.end()
+});
+
+function updateDatabase(response) {
+  var req = coap.request({
+    host: 'coap.thethings.io',
+    pathname: '/v2/things/TOKEN_ID',
+    method: 'POST'
+  });
+  console.log('response ', response);
+  var payload = {
+    values: [{
+      key: "demo_resource",
+      value: response
+    }]
+  };
+  req.write(JSON.stringify(payload));
+
+  req.on('response', function(res) {
+    var response = res.pipe(process.stdout);
+    console.log('thethings.io response: ', response);
+  })
+
+  req.end()
+}
