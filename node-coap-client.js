8e811448413ff162b1769f29c1eec57b1082b5de
playola <pol944li@gmail.com>
2018-06-07 19:18:58 +0200

    on pipe

--- node-coap-client.js
+++ node-coap-client.js
@@ -28,14 +28,20 @@ server.listen(function() {
       method: 'GET'
     })
     .on('response', function(res) {
-      console.log('error code ', res.code);
       if (res.code !== '2.05') return process.exit(1);
 
+      res.on('pipe', function(src) {
+        console.log(src);
+      })
+      /*
+      console.log('error code ', res.code);
       res.pipe(bl(function(err, data) {
         var json = JSON.parse(data)
         console.log('json ', json)
         process.exit(0)
-      }))
+      }));
+      */
+
     })
     .end()
 });
